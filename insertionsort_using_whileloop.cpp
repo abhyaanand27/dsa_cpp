b016c24a612492e92580777a1336a36ee4c33a98
abhyaanand27 <abhyaanand2004@gmail.com>
2023-08-21 09:12:26 +0530

    new commit

--- insertionsort_using_whileloop.cpp
+++ insertionsort_using_whileloop.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 using namespace std;
-
-
-
+ 
 void insertionSort(int arr[], int n) {
     for(int i = 0; i<n; i++) {
 	    int temp = arr[i];
-        int j;
-	    for(j = i-1; j>=0; j--) {
-	        if(arr[j] > temp) {
-		        arr[j+1] = arr[j];
-	        }
-	        else {
-		        break;
-	        }
-	    }
-	    arr[j+1] = temp;
+        int j = i-1;
+	    while(j>=0 && arr[j] > temp) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
     }
 }
 
@@ -27,7 +21,7 @@ int main() {
     int *arr = new int[n];
     for(int i = 0; i<n; i++) {
         cin >> arr[i];
-    }
+    } 
     insertionSort(arr, n);
     for(int j = 0; j<n; j++) {
        cout << arr[j] << " ";
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,15 @@
-cout << isHappy(n) << endl;
+void insertionSort(int arr[], int n) {
+    for(int i = 0; i<n; i++) {
+	    int temp = arr[i];
+        int j;
+	    for(j = i-1; j>=0; j--) {
+	        if(arr[j] > temp) {
+		        arr[j+1] = arr[j];
+	        }
+	        else {
+		        break;
+	        }
+	    }
+	    arr[j+1] = temp;
+    }
+}
