4dcff489e21abd181465a91e392758fc87ad2146
abhyaanand27 <abhyaanand2004@gmail.com>
2023-08-25 09:39:36 +0530

    new commit

--- mergesort.cpp
+++ mergesort.cpp
@@ -18,7 +18,7 @@ void merge(int *arr, int s, int e){
     }
 
     mainArrayIndex = mid+1;
-    for( int i =0; i < len1; i++){
+    for( int i =0; i < len2; i++){
         second[i] = arr[mainArrayIndex++];
     } 
 
